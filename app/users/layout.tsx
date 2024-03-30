d8430827b36c341120af8e52ee5f386c44dc27cf
Dhanushnetha <dk40828@gmail.com>
2024-03-31 02:21:28 +0530

    SideBar

---
+++ app/users/layout.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import SideBar from '../components/sidebar/SideBar'
+
+const UsersLayout = async ({children}: {children: React.ReactNode}) => {
+  return (
+    <SideBar>
+        <div className='h-full'>{children}</div>
+    </SideBar>
+  )
+}
+
+export default UsersLayout
