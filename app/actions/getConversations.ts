0db92228100fe831db9c6ba0e8581b0c25a4f828
Dhanushnetha <dk40828@gmail.com>
2024-03-31 19:27:42 +0530

    UserList & Conversation setup

---
+++ app/actions/getConversations.ts
@@ -0,0 +1,39 @@
+import { getCurrentUser } from "./getCurrentUser"
+
+import prisma from '@/app/libs/prismadb'
+
+const getConversations = async()=>{
+    const currentUser = await getCurrentUser();
+
+    if(!currentUser?.id){
+        return [];
+    }
+
+    try {
+        const conversations = await prisma?.conversation.findMany({
+            where:{
+                userIds:{
+                    has: currentUser.id
+                }
+            },
+            orderBy:{
+                lastMessageAt: "desc"
+            },
+            include:{
+                users: true,
+                messages: {
+                    include:{
+                        sender: true,
+                        seen: true
+                    }
+                }
+            }
+        })
+
+        return conversations;
+    } catch (error: any) {
+        return [];
+    }
+}
+
+export default getConversations;
--- app/users/layout.tsx
+++ app/users/layout.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import SideBar from '../components/sidebar/SideBar'
+import getUsers from '../actions/getUsers'
+import UserList from './components/UserList';
 
 const UsersLayout = async ({children}: {children: React.ReactNode}) => {
+  
+  const users = await getUsers();
+
   return (
     <SideBar>
-        <div className='h-full'>{children}</div>
+        <div className='h-full'>
+          <UserList items={users} />
+          {children}
+        </div>
     </SideBar>
   )
 }
